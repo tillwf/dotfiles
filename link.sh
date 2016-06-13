75b7fccbc3669d24f2b1f4d7737efcb12715e287
Till <till@monsieurdrive.com>
2016-06-13 15:37:30 +0200

    Update link.sh to copy sublime config file

--- link.sh
+++ link.sh
@@ -12,3 +12,6 @@ for file in $dotfiles;do
 [ -s "$HOME/.${file}" ] && unlink $HOME/.${file}
 cp $env_dir/$file $HOME/.${file}
 done
+
+cp "Preferences.sublime-settings" ~/.config/sublime-text-3/Packages/User/.
+cp "Default (Linux).sublime-keymap" ~/.config/sublime-text-3/Packages/User/.
